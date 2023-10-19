b5fd84aead1a7f64712c829a49382f2930ee189e
Leonardo Gabriel Rosa <leogrosa@leogrosa-macbook-air.local>
2023-10-19 00:06:47 -0300

    refact(dobras.py): separando sweep do calculo original

--- dobras.py
+++ dobras.py
@@ -22,25 +22,29 @@ e[0] = e0
 
 columns = ['c', 'l', 'e', 'i']
 
-def simulate(m, n, i_max, c, l, espessura):
+def dimension_sweep(m, n, i_max, c, l, espessura):
     e = espessura.copy()
+    i = calculate_foldings(i_max, c[:,m], l[:,n], e)
+    current_results = np.array([c[0, m], l[0, n], e[i], i]).T
+    return current_results
+
+def calculate_foldings(i_max, c, l, e):
     for i in range(i_max-1):
-        if c[i,m] > l[i,n]:
-            c[i+1,m] = c[i,m]/2 - e[i]
-            l[i+1,n] = l[i,n]
+        if c[i] > l[i]:
+            c[i+1] = c[i]/2 - e[i]
+            l[i+1] = l[i]
         else:
-            c[i+1,m] = c[i,m]
-            l[i+1,n] = l[i,n]/2 - e[i]
-        if any([value < 0 for value in [c[i+1,m], l[i+1,n]]]):
-            current_results = np.array([c[0, m], l[0, n], e[i], i]).T 
-            return current_results
+            c[i+1] = c[i]
+            l[i+1] = l[i]/2 - e[i]
+        if any([value < 0 for value in [c[i+1], l[i+1]]]):
+            return i
         e[i+1] = 2*e[i]
 
 threads_results = []
 with concurrent.futures.ThreadPoolExecutor() as executor:
     for m, comprimento in enumerate(c[0,:]):
         for n, largura in enumerate(l[0,:]):
-            threads_results.append(executor.submit(simulate, m, n, i_max, c, l, e))
+            threads_results.append(executor.submit(dimension_sweep, m, n, i_max, c, l, e))
     executor.shutdown()
     concurrent.futures.wait(threads_results)
 
@@ -53,7 +57,6 @@ ax = fig.add_subplot(111, projection='3d')
 
 X, Y = np.meshgrid(results['c'].unique(), results['l'].unique())
 Z = results.pivot(index='c', columns='l', values='i').values
-
 
 ax.plot_surface(X, Y, Z, cmap='viridis')
 ax.set_xlabel('Comprimento')
