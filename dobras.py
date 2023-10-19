a57648f1a560454d2819fb0b8b7be2458a4f546a
Leonardo Gabriel Rosa <leogrosa@leogrosa-macbook-air.local>
2023-10-18 23:49:31 -0300

    feat(dobras.py): executando o calculo em paralelo para diferentes tamanhos

--- dobras.py
+++ dobras.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 import pdb
 from mpl_toolkits.mplot3d import Axes3D
+import concurrent.futures
 
 i_max = 50
-step = 100
-max_size = 500
+step = 20
+max_size = 10000
 c_max = max_size + step
 l_max = max_size + step
 e0 = 0.1
@@ -20,26 +21,31 @@ e = np.zeros(i_max)
 e[0] = e0
 
 columns = ['c', 'l', 'e', 'i']
-results = pd.DataFrame(columns=columns)
 
-# Processo iterativo
-for m, comprimento in enumerate(c[0,:]):
-    for n, largura in enumerate(l[0,:]):
-        for i in range(i_max-1):
-            if c[i,m] > l[i,n]:
-                c[i+1,m] = c[i,m]/2 - e[i]
-                l[i+1,n] = l[i,n]
-            else:
-                c[i+1,m] = c[i,m]
-                l[i+1,n] = l[i,n]/2 - e[i]
-            if any([value < 0 for value in [c[i+1,m], l[i+1,n]]]):
-                current_results = pd.DataFrame([comprimento, largura, e[i], i]).T 
-                current_results.columns = columns
-                results = pd.concat([results, \
-                                         current_results],\
-                                         ignore_index=True)
-                break
-            e[i+1] = 2*e[i]
+def simulate(m, n, i_max, c, l, espessura):
+    e = espessura.copy()
+    for i in range(i_max-1):
+        if c[i,m] > l[i,n]:
+            c[i+1,m] = c[i,m]/2 - e[i]
+            l[i+1,n] = l[i,n]
+        else:
+            c[i+1,m] = c[i,m]
+            l[i+1,n] = l[i,n]/2 - e[i]
+        if any([value < 0 for value in [c[i+1,m], l[i+1,n]]]):
+            current_results = np.array([c[0, m], l[0, n], e[i], i]).T 
+            return current_results
+        e[i+1] = 2*e[i]
+
+threads_results = []
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for m, comprimento in enumerate(c[0,:]):
+        for n, largura in enumerate(l[0,:]):
+            threads_results.append(executor.submit(simulate, m, n, i_max, c, l, e))
+    executor.shutdown()
+    concurrent.futures.wait(threads_results)
+
+final_results = [r.result() for r in threads_results]
+results = pd.DataFrame([r.result() for r in threads_results], columns=columns)
 
 # Plotagem
 fig = plt.figure()
